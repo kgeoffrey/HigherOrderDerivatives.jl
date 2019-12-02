9c136f0807df0828aa6266fb26fe046efb08ef18
Geoffrey Kasenbacher <gkasenbacher@gmail.com>
2019-12-02 16:37:06 +0100

    create

---
+++ Dual.jl
@@ -0,0 +1,76 @@
+### simple Automatic Differentiation with Dual Numbers
+using Distributions, LinearAlgebra
+
+struct Dual{T} <: Number
+    f::Union{T, Dual}
+    g::Union{T, AbstractArray}
+end
+DualandReal = Union{Dual, Real}
+
+### Differentiation rules via overloading ###
+import Base: +,/,*,-,^,adjoint, convert, promote_rule
+import Base: +,/,*,-,^, convert, promote_rule
+
++(x::Dual, y::Dual) = Dual(x.f + y.f, x.g + y.g)
++(x::Dual, y::Real) = Dual(x.f + y, x.g)
++(y::Real, x::Dual) = Dual(x.f + y, x.g)
+-(x::Dual, y::Dual) = Dual(x.f - y.f, x.g - y.g)
+-(x::Dual) = Dual(-x.f, -x.g)
+-(x::Dual, y::Real) = Dual(x.f -y, x.g)
+-(y::Real, x::Dual) = Dual(y-x.f, -x.g)
+*(x::Dual, y::Dual) = Dual(x.f*y.f, x.f*y.g + y.f*x.g)
+*(x::Dual, y::Real) = Dual(x.f*y, x.g*y)
+*(y::Real, x::Dual) = Dual(x.f*y, x.g*y)
+/(x::Dual, y::Dual) = Dual(x.f/y.f, (y.f*x.g - x.f*y.g)/y.f^2)
+/(y::Real, x::Dual) = Dual(y/x.f, (-y*x.g) / x.f^2)
+/(x::Dual, y::Real) = Dual(x.f/y, x.g/y)
+^(x::Dual, k::Real) = Dual(x.f^k, (x.g * k) * x.f ^ (k-1))
+^(x::Dual, k::Int) = Dual(x.f^k, (k * x.f ^ (k-1)) * x.g)
+Base.exp(x::Dual) = Dual(exp(x.f), x.g * exp(x.f))
+Base.sqrt(x::Dual) = Dual(sqrt(x.f), x.g / (2 * sqrt(x.f)))
+Base.log(x::Dual) = Dual(log(x.f), x.g/x.f)
+Distributions.cdf(d, x::Dual) = Dual(cdf(d, x.f), pdf(d, x.f) * x.g)
+Base.adjoint(x::Dual) = Dual(adjoint(x.f), adjoint(x.g))
+LinearAlgebra.dot(x::Dual, y::Dual) = Dual(dot(x.f,y.f), x.f * y.g + y.f * x.g)
+Base.zero(x::Dual) = Dual(zero(x.f), zero(x.g))
+
+### conversion, promotion rules ###
+convert(::Type{Dual}, x::Real) = Dual(x, one(x))
+convert(::Type{Dual}, x::AbstractArray) = DualArray(x)
+convert(::Type{Array}, x::Real) = [x]
+Dual(x) = convert(Dual, x)
+promote_rule(::Type{Dual}, ::Type{<:Number}) = Dual
+
+function DualArray(x)
+    l = length(x)
+    eye = I(l)
+    collect = []
+    for i in 1:l
+        push!(collect, Dual(x[i], view(eye,i,:,)))
+    end
+    return collect
+end
+
+
+### Recursive functions for getting the derivativ  ###
+function chain(x::DualandReal, n::Int)
+    if n == 1
+        return Dual(x, 1)
+    else
+        return chain(Dual(x, 1), n-1)
+    end
+end
+
+
+function dechain(x::DualandReal)
+    if x isa Real
+        return x
+    else
+        dechain(x.g[1])
+    end
+end
+
+
+function derivative(f::Function, x::Real, n::Int)
+    return dechain(f(chain(x,n)))
+end

3d3b57efd990022474f3d5cc3ceed462eb91d9a2
Geoffrey Kasebacher <gkasenbacher@gmail.com>
2019-12-03 02:16:06 +0100

    clean

--- src/DualAlgebra.jl
+++ src/DualAlgebra.jl
@@ -24,15 +24,14 @@ import Base: +,/,*,-,^, convert, promote_rule
 /(x::Dual, y::Dual) = Dual(x.f/y.f, (y.f*x.g - x.f*y.g)/y.f^2)
 /(y::Real, x::Dual) = Dual(y/x.f, (-y*x.g) / x.f^2)
 /(x::Dual, y::Real) = Dual(x.f/y, x.g/y)
-^(x::Dual, k::Real) = Dual(x.f^k, (x.g * k) * x.f ^ (k-1))
-^(x::Dual, k::Int) = Dual(x.f^k, (k * x.f ^ (k-1)) * x.g)
+^(x::Dual, k::Real) = Dual(x.f^(k), (x.g * k) * x.f ^ (k-1))
+^(x::Dual, k::Int) = Dual(x.f^(k), (k * x.g) * x.f ^ (k-1))
 Base.exp(x::Dual) = Dual(exp(x.f), x.g * exp(x.f))
 Base.sqrt(x::Dual) = Dual(sqrt(x.f), x.g / (2 * sqrt(x.f)))
 Base.log(x::Dual) = Dual(log(x.f), x.g/x.f)
 
 Base.sin(x::Dual) = Dual(sin(x.f), cos(x.f)*x.g)
 Base.cos(x::Dual) = Dual(cos(x.f), -sin(x.f)*x.g)
-Base.tan(x::Dual) = Dual(tan(x.f), (sec(x.f)^2)*x.g)
 
 Distributions.cdf(d, x::Dual) = Dual(cdf(d, x.f), pdf(d, x.f) * x.g)
 Base.adjoint(x::Dual) = Dual(adjoint(x.f), adjoint(x.g))
--- src/HigherOrderDerivatives.jl
+++ src/HigherOrderDerivatives.jl
@@ -2,7 +2,7 @@
 
 module HigherOrderDerivatives
 
-export chain, dechain, derivative
+export chain, dechain, derivative, Dual
 ### Recursive functions for getting the derivativ  ###
 include("DualAlgebra.jl")
 
